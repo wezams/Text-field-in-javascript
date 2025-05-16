761c4fafe467783bb9a021399abe94a91e61cfbd
wezams <148435240+wezams@users.noreply.github.com>
2025-05-16 22:10:50 +0200

    detection complete

--- textruta.js
+++ textruta.js
@@ -1,18 +1,33 @@
 let textruta = null;
 let text = "";
+spacePos = 0;
+spanContent = "";
 document.addEventListener("DOMContentLoaded", function(){
     textruta = document.getElementById("textruta");
     document.onkeydown = function(e){
-        console.log(e);
-        if(e.key == ' '){
-            text = text + " ";
-        }
-        else if(e.key == 'Alt'){
+        if(e.key == 'Alt'){
             console.log(text);
         }
         else{
-            text = text + e.key;
+            spanContent = spanContent + e.key;
         }
-        textruta.innerHTML = text;
+        if(e.key == ' '){
+            if(spacePos == text.length - 1){
+                spacePos = text.length;
+                console.log(spacePos);
+            }
+            else{
+                spacePos = text.length;
+                if(spanContent != "röd "){
+                    text = text + "<span>" + spanContent + "</span>";
+                }
+                else{
+                    text = text + "<span id='röd'>" + spanContent + "</span>"
+                }
+                spanContent = "";
+            }
+            //text = text + " ";
+        }
+        textruta.innerHTML = text + spanContent;
     };
 });
